761f18dd5ddcae672cf65a7c7b33da3006af50fd
Sam Pupke <sampupke@gmail.com>
2024-01-24 13:17:45 -0500

    first lecture in 2150

---
+++ Intentional Design/IntentionalDesign.java
@@ -0,0 +1,10 @@
+public class IntentionalDesign {
+    //what is ensuring that every object or module has a specific role
+        //separation of concerns
+        //
+        //what is controlling access to data and operations
+        //information hiding
+        //
+        //what is grouping related data and operations together in one object
+        //encapsulation
+}
---
+++ Intentional Design/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        //what is ensuring that every object or module has a specific role
+        //separation of concerns
+        //
+        //what is controlling access to data and operations
+        //information hiding
+        //
+        //what is grouping related data and operations together in one object
+        //encapsulation
+    }
+}
