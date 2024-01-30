42865578663bf7d7431a120a779ac9131ded94fe
Sam Pupke <sampupke@gmail.com>
2024-01-30 17:57:50 -0500

    functional and non functional requirement statements

--- Intentional Design/IntentionalDesign.java
+++ Intentional Design/IntentionalDesign.java
@@ -7,4 +7,50 @@ public class IntentionalDesign {
         //
         //what is grouping related data and operations together in one object
         //encapsulation
+
+        //functional requirements
+        //describe the interactions between the program and its environment and its users
+
+        //user story form
+        //as a <ROLE> I <want/need/can> <WHAT> so that <WHY>
+
+        //as an Amazon shopper, I need to be able to browse the store so that I can buy things
+        //as an Amazon shopper, I want to be able to access all I'm shopping for (cart), so that I have convenience
+        //As an amazon shopper, I can utilize a search bar, so that I can find what I need
+
+
+
+        //non functional requirements
+        //requirements of the program that arent actions or that the user doesnt interact with directly
+
+        //FURPS+
+        //Functional Requirements
+        //Usability
+        //Reliablity
+        //Performance
+        //Supportability
+        //+Constraints and Pseudo Requirements
+            //implementation
+            //interface
+            //operations
+            //packaging
+            //legal
+
+        //non functional requirements are often ones we forget because theyre considered as "given"
+        //still important to specify these givens so everyone is on the same page
+        //NFreq: the system must run on windows and unix/linux
+            //given, but have to let the mac users know their OS is less supported
+
+
+        //as a user i can utilize a search feature
+        //bad, no why. user nondescript
+
+        //as a player i can enter my real name for my character so i can get a better snse of realism
+        //functional
+
+        //the game should run on all modern gaming consoles
+        ///nonfuncitonal
+
+        //the game should feature differently themed worlds, so the player doesnt get bored
+        //functional
 }
